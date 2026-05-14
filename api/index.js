9c2c5ce2e0ae634803b27dd1741fccbdf9e73cfd
jvng24 <jnguye31@terpmail.umd.edu>
2026-05-14 16:18:44 -0400

    fix vercel api structure

--- api/index.js
+++ api/index.js
@@ -10,12 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// API routes
 app.use("/api/moods", moodRoutes);
 app.use("/api/weather", weatherRoutes);
 
-// frontend
+// serve frontend
 app.use(express.static(path.join(__dirname, "../client")));
 
+// homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/home.html"));
 });
