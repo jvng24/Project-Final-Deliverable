e38bb917c9be92db9753a8e0a17d26b402559333
jvng24 <jnguye31@terpmail.umd.edu>
2026-05-13 23:48:36 -0400

    fix vercel function structure

--- api/index.js
+++ api/index.js
@@ -1,28 +1,23 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-const moodRoutes = require("./routes/moods");
-const weatherRoutes = require("./routes/weather");
+const moodRoutes = require("../server/routes/moods");
+const weatherRoutes = require("../server/routes/weather");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// serve frontend
-app.use(express.static(path.join(__dirname, "../client")));
-
-// API routes
 app.use("/api/moods", moodRoutes);
 app.use("/api/weather", weatherRoutes);
 
-// homepage route
+// frontend
+app.use(express.static(path.join(__dirname, "../client")));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/home.html"));
 });
 
-// IMPORTANT: no app.listen here
 module.exports = app;
